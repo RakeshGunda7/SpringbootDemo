cf071bc6db894017aa8d15257bbe49813a3d36e1
Rakesh Gunda <gunda.rakesh777@gmail.com>
2023-09-26 16:49:09 -0500

    PrimeNumbers

--- SpringBootDemoProject/src/main/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplication.java
+++ SpringBootDemoProject/src/main/java/com/example/SpringBootDemoProject/SpringBootDemoProjectApplication.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class SpringBootDemoProjectApplication {
@@ -19,4 +23,23 @@ public class SpringBootDemoProjectApplication {
 		return String.format("Hello %s!", name);
 	}
 
+	@GetMapping("/PrimeNumbers")
+	public List<Integer> getPrimes(@RequestParam(value = "upperBound", defaultValue = "10") int upperBound) {
+		List<Integer> primes = new ArrayList<>();
+
+		if (upperBound <= 1) return Collections.emptyList();
+
+		for (int x = 1; x <= upperBound; x++) {
+			if (x == 1 || x == 0) continue;
+			int flg = 1;
+			for (int y = 2; y <= x / 2; ++y) {
+				if (x % y == 0) {
+					flg = 0;
+					break;
+				}
+			}
+			if (flg == 1) primes.add(x);
+		}
+		return primes;
+	}
 }
